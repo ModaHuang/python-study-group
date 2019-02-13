0e61f35d81b7f6be2fbf32a8a098a27256c130a0
ModaHuang <moda.huang@kkday.com>
2019-02-13 12:02:17 +0800

    fix bug

--- homework/ch5_hw_v2.py
+++ homework/ch5_hw_v2.py
@@ -1,11 +1,13 @@
 """
 1. 解釋 stack 跟 stack 是什麼? 他們的差別是?
+
 stack : 先進後出
     ↘ ↗
     |-|
     |-|
     |-|
     |_|
+
 queue : 先進先出
      ↑
     |-|
@@ -13,6 +15,7 @@ queue : 先進先出
     |-|
     |-|
      ↑
+
 """
 
 """
@@ -101,93 +104,119 @@ def debug(variable_name, variable):
     print(variable_name, "=", variable, "\n    type =", type(variable))
 
 
+def kqueue_pop_all(list, temp_list):
+    queue = kqueue.kqueue()
+    temp_queue = kqueue.kqueue()
+
+    queue.queue = list
+    temp_queue.queue = temp_list
+
+    while None != temp_queue.pop():
+        queue.push(temp_queue.pop())
+
+
 input_list = [2, 3, 1, 9, 22, 31, 8, 7, 45, 9]
 debug("input_list", input_list)
 
 kqueue1 = kqueue.kqueue()
-kqueue_temp = kqueue.kqueue()
+temp_kqueue = kqueue.kqueue()
 
 for element in input_list:
     if kqueue1.size() == 0:
         kqueue1.push(element)
-        for x in kqueue_temp.queue:
-            kqueue1.push(x)
-        kqueue_temp = kqueue.kqueue()
+        kqueue_pop_all(kqueue1.queue, temp_kqueue.queue)
 
-    elif None == kqueue1.back() or element >= kqueue1.back():
+    elif element >= kqueue1.back():
         kqueue1.push(element)
-        for x in kqueue_temp.queue:
-            kqueue1.push(x)
-        kqueue_temp = kqueue.kqueue()
+        kqueue_pop_all(kqueue1.queue, temp_kqueue.queue)
 
-    else:
+    else:  # element < kqueue1.back()
         if element <= kqueue1.front():
-            kqueue_temp.push(element)
+            temp_kqueue.push(element)
+            debug("136 kqueue1.queue", kqueue1.queue)
+            debug("137 temp_kqueue.queue", temp_kqueue.queue)
+            # kqueue_pop_all(temp_kqueue.queue, kqueue1.queue)
             for x in kqueue1.queue:
-                kqueue_temp.push(x)
+                temp_kqueue.push(x)
             kqueue1 = kqueue.kqueue()
-            for x in kqueue_temp.queue:
-                kqueue1.push(x)
-            kqueue_temp = kqueue.kqueue()
+            debug("139 kqueue1.queue", kqueue1.queue)
+            debug("140 temp_kqueue.queue", temp_kqueue.queue)
+            kqueue_pop_all(kqueue1.queue, temp_kqueue.queue)
 
         else:
             while element > kqueue1.front():
-                kqueue_temp.push(kqueue1.pop())
+                temp_kqueue.push(kqueue1.pop())
 
             else:
-                kqueue_temp.push(element)
+                temp_kqueue.push(element)
                 for x in kqueue1.queue:
-                    kqueue_temp.push(x)
+                    temp_kqueue.push(x)
                 kqueue1 = kqueue.kqueue()
-                for x in kqueue_temp.queue:
+                for x in temp_kqueue.queue:
                     kqueue1.push(x)
-                kqueue_temp = kqueue.kqueue()
+                temp_kqueue = kqueue.kqueue()
+
+
+        # while kqueue1.size() == 0 or element < kqueue1.back():
+        #     temp_kqueue.push(kqueue1.pop())
+        #     debug("136 kqueue1.queue", kqueue1.queue)
+        #     debug("137 temp_kqueue", temp_kqueue.queue)
+        #     if kqueue1.size() == 0:
+        #         kqueue1.push(element)
+        #         debug("140 kqueue1", kqueue1.queue)
+        #         debug("141 temp_kqueue", temp_kqueue.queue)
+        #
+        #     elif element >= kqueue1.back():
+        #         kqueue1.push(element)
+        #         kqueue_pop_all(kqueue1.queue, temp_kqueue.queue)
+        #         debug("146 kqueue1", kqueue1.queue)
+        #         debug("147 temp_kqueue", temp_kqueue.queue)
+        #         break
 
 
 debug("kqueue1.queue", kqueue1.queue)
-# debug("kqueue_temp.queue", kqueue_temp.queue)
+# debug("temp_kqueue.queue", temp_kqueue.queue)
 
 
 kstack1 = kstack.kstack()
-kstack_temp = kstack.kstack()
+temp_kstack = kstack.kstack()
 
 for element in input_list:
     if kstack1.size() == 0:
         kstack1.push(element)
-        for x in kstack_temp.stack:
+        for x in temp_kstack.stack:
             kstack1.push(x)
-        kstack_temp = kstack.kstack()
+        temp_kstack = kstack.kstack()
 
     elif None == kstack1.top() or element >= kstack1.top():
         kstack1.push(element)
-        kstack_temp.push(kstack1.pop())
-        for x in kstack_temp.stack:
+        for x in temp_kstack.stack:
             kstack1.push(x)
-        kstack_temp = kstack.kstack()
+        temp_kstack = kstack.kstack()
 
     else:
         if element <= kstack1.stack[0]:
-            kstack_temp.push(element)
+            temp_kstack.push(element)
             for x in kstack1.stack:
-                kstack_temp.push(x)
+                temp_kstack.push(x)
             kstack1 = kstack.kstack()
-            for x in kstack_temp.stack:
+            for x in temp_kstack.stack:
                 kstack1.push(x)
-            kstack_temp = kstack.kstack()
+            temp_kstack = kstack.kstack()
 
         else:
             while kstack1.size() == 0 or element > kstack1.stack[0]:
-                kstack_temp.push(kstack1.pop())
+                temp_kstack.push(kstack1.pop())
                 if kstack1.size() == 0:
                     kstack1.push(element)
 
                 if element >= kstack1.top():
                     kstack1.push(element)
-                    while kstack_temp.size() != 0:
-                        kstack1.push(kstack_temp.pop())
-                    kstack_temp = kstack.kstack()
+                    for x in temp_kstack.stack:
+                        kstack1.push(x)
+                    temp_kstack = kstack.kstack()
                     break
 
 
 debug("kstack1.stack", kstack1.stack)
-# debug("kstack_temp.stack", kstack_temp.stack)
+# debug("temp_kstack.stack", temp_kstack.stack)
